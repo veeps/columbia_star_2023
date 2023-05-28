839bf4cbeb9713731687a7a136af9a2b8320d4f7
veeps <vpeng12@gmail.com>
2023-05-28 18:34:22 -0400

    prepping file for workshop

--- workshop_2/02_cereals.R
+++ workshop_2/02_cereals.R
@@ -25,20 +25,14 @@ ui <- fluidPage(
   fluidRow(style="padding:40px; background: #f2f2f2",
            h3("Nutritional Density ", align="center"),
            p("Let's start by ranking cereals based on their nutritional density. Which ones have more of the 'good stuff'? ", align="center")
-  ), # end fluid row
+  ) # end fluid row
   
-  # Bar chart section -------------------------------------------------------
-  fluidRow(
-    column(3,radioButtons(inputId="bar_var", #references the input to server
-                          label = h3("Select Variable"), # text that appears on UI
-                          choices=nutrient_dense,
-                          selected="Protein")),
-    column(9, plotOutput("bar_plot"))
-  ),
+  # Bar chart section -----------------------------------------------------
+  
   # Data table section ----------------------------------------------------
-  DTOutput("table"),
+  
   # Scatter plot section --------------------------------------------------
-  plotlyOutput("scatter_plot")
+  
   
 ) # end UI
 
@@ -50,27 +44,23 @@ server <- function(input, output, session) {
   # Bar chart section -------------------------------------------------------
   
   # render barchart
-  output$bar_plot <- renderPlot({
-    ggplot(df |> arrange(desc(.data[[input$bar_var]])) |> head(10),
-           aes(y=.data[[input$bar_var]], 
-               x=Name)) + 
-      geom_bar(stat="identity", fill = "#0add8c") +
-      theme(axis.title.x=element_blank())
-  })
+  # output$bar_plot <- renderPlot({
+  #   ggplot(df |> arrange(desc(Protein)) |> head(10),
+  #          aes(y=Protein, 
+  #              x=Name)) + 
+  #     geom_bar(stat="identity", fill = "#0add8c") +
+  #     theme(axis.title.x=element_blank())
+  # })
   
   # Data table section ----------------------------------------------------
   
   # render table
-  output$table <- renderDT(df |> arrange(desc(Protein)) |> head(10))
+  # output$table <- renderDT()
   
   # Scatter plot section --------------------------------------------------
   
   # render scatterplot
-  output$scatter_plot <- renderPlotly({plot_ly(data = df, x= ~Protein, y = ~Sugars,
-                                               type = "scatter", mode = "markers",
-                                               hovertemplate = paste0(
-                                                 df$Name, "<br>", "Protein", ": %{x}<br>", "Sugars", ": %{y}<extra></extra>")) 
-  })
+  # output$scatter_plot <- renderPlotly({ })
   
   
 }
